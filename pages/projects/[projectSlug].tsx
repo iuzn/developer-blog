856326436e98b86ab6f3bfefb9f19dcc7b92a22f
İbrahim Uzun <info@ibrahimuzun.com>
2021-01-23 13:13:41 +0300

    Redeploy to fix error

--- pages/projects/[projectSlug].tsx
+++ pages/projects/[projectSlug].tsx
@@ -69,7 +69,7 @@ export const getStaticProps: GetStaticProps<
   }
 }
 
-const BlogPosts: React.FC<PostProps> = ({ project, blocks }) => {
+export const BlogPosts: React.FC<PostProps> = ({ project, blocks }) => {
   const router = useRouter()
   if (router.isFallback) {
     return (
@@ -131,4 +131,3 @@ const BlogPosts: React.FC<PostProps> = ({ project, blocks }) => {
     </>
   )
 }
-export default BlogPosts
