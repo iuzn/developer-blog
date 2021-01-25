2527d19cb6304b3c0065c394dd5f6e2400753bdf
İbrahim Uzun <info@ibrahimuzun.com>
2021-01-25 22:49:16 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,9 +30,9 @@ export const getStaticProps: GetStaticProps<AppProps> = async () => {
     props: {
       blogpost: blogpost.filter((p) => p.published),
       project: project.filter((p) => p.published),
-      bookmark: bookmark
+      bookmark: bookmark.filter((p) => p.published)
     },
-    revalidate: 1
+    revalidate: 10
   }
 }
 
