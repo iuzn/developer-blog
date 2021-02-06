dfb20c36fa79a86867c84413b83d408962cf3785
İbrahim Uzun <info@ibrahimuzun.com>
2021-02-07 01:22:43 +0300

    Rss added

---
+++ core/rss.ts
@@ -0,0 +1,25 @@
+import { BlogPost } from "../types/blog";
+
+const generateRssItem = (post: BlogPost): string => `
+<item>
+  <guid>https://timo.sh/blog/${post.slug}</guid>
+  <title>${post.title}</title>
+  <link>https://timo.sh/blog/${post.slug}</link>
+  <description>${post.preview}</description>
+  <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+</item>
+`;
+
+export const generateRss = (posts: BlogPost[]): string => `
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+  <title>Ibrahium Uzun – Blog</title>
+  <link>https://dev.ibrahimuzun.com/blog</link>
+  <description>En güncel geliştirici içeriklerini bu adresten bulabilirsiniz.</description>
+  <language>tr</language>
+  <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+  <atom:link href="https://dev.ibrahimuzun.com/api/blog.xml" rel="self" type="application/rss+xml"/>
+  ${posts.map(generateRssItem).join("")}
+</channel>
+</rss>
+`;
