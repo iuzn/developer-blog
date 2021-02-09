868b1065bcf7e7503fd8cb431aed1917a5fd0ba9
İbrahim Uzun <info@ibrahimuzun.com>
2021-02-09 22:08:01 +0300

    Tailwindcss config sonrası düzenleme

--- components/newsletter/index.js
+++ components/newsletter/index.js
@@ -20,21 +20,21 @@ const Newsletter = () =>  {
 
   return (
     <div className="flex flex-col items-center mt-8">
-      <h2 className="title-primary">
+      <h2 className="text-3xl font-bold text-center">
         E-bültenime abone ol!
       </h2>
-      <p className="text-primary">
+      <p className="mt-2 font-light w-4/5 text-center leading-relaxed">
         En son yazdığım blog yazılarından haberdar olmak için e-postanızı girin.
       </p>
-      <form className="border-primary form-primary">
+      <form className="flex border-primary sm:block mt-6 rounded-full ">
         <input
-          className="input-primary"
+          className="py-3 px-3 border-none bg-transparent w-48 sm:w-64 focus:ring-0 focus:outline-none"
           type="text"
           placeholder="E-posta Girin"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         /><input type="button" value="Abone ol"
-          onClick={subscribe} className="button-primary form-button"/>
+          onClick={subscribe} className="button-primary rounded text-white py-3 px-6 cursor-pointer"/>
       </form>
       {state === "ERROR" && (
         <p className="w-1/2 mt-2 text-red-600">{errorMessage}</p>
