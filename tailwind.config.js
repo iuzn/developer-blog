47a17767568b412babb7547e5f1b565a4c34787d
İbrahim Uzun <info@ibrahimuzun.com>
2021-02-09 22:05:00 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
     "./pages/**/*.tsx",
     "./components/*.tsx",
     "./components/**/*.tsx",
+    "./components/*.js",
+    "./components/**/*.js",
     "./pages/*.js",
     "./components/footer-box/*.js"
   ],
