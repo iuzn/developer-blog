f1fe0bdf46a16eded5c535318fc1cb6c01fbf1bc
İbrahim Uzun <info@ibrahimuzun.com>
2021-02-18 00:25:41 +0300

    Sitemap ve robots düzenlendi

--- core/sitemap.ts
+++ core/sitemap.ts
@@ -6,14 +6,18 @@ const EXTERNAL_DATA_URL = 'https://dev.ibrahimuzun.com/';
 const generateSitemapBlog = (post: BlogPost,): string => `
 <url>
    <loc>${EXTERNAL_DATA_URL+"blog/"+ post.slug}</loc>
+   <lastmod>${post.date}</lastmod>
 </url>
 `;
 const generateSitemapProject = (project:Project): string => `
 <url>
    <loc>${EXTERNAL_DATA_URL+"project/"+ project.slug}</loc>
+   		<lastmod>${project.date}</lastmod>
+
 </url>
 `;
 export const generateSitemap = (posts: BlogPost[],project:Project[] ): string => `
+<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${posts.map(generateSitemapBlog).join("")}
   ${project.map(generateSitemapProject).join("")}
--- pages/robots.txt.tsx
+++ pages/robots.txt.tsx
@@ -3,7 +3,48 @@ import React, { Component } from "react";
 export default class Robots extends Component {
     static getInitialProps({ res }: { res: any}) {
         res.setHeader("Content-Type", "text/plain");
-        res.write(`Sitemap: https://dev.ibrahimuzun.com/api/sitemap.xml
+        res.write(`#Google Search Engine Robot
+User-agent: Googlebot
+Allow: /*
+Allow: /blog/*
+Allow: /projeler/*
+
+Disallow: /_next/*
+Disallow: /api/*
+Disallow: /_next/*
+
+#Yahoo! Search Engine Robot
+User-agent: Slurp
+Allow: /*
+Allow: /blog/*
+Allow: /projeler/*
+
+Disallow: /_next/*
+Disallow: /api/*
+Disallow: /_next/*
+
+#Yandex Search Engine Robot
+User-agent: Yandex
+Allow: /*
+Allow: /blog/*
+Allow: /projeler/*
+
+Disallow: /_next/*
+Disallow: /api/*
+Disallow: /_next/*
+
+
+#Bing Search Engine Robot
+User-agent: bingbot
+Allow: /*
+Allow: /blog/*
+Allow: /projeler/*
+
+Disallow: /_next/*
+Disallow: /api/*
+Disallow: /_next/*
+
+# Every bot that might possibly read and respect this file.
 User-agent: *
 Allow: /*
 Allow: /blog/*
@@ -11,7 +52,8 @@ Allow: /projeler/*
 
 Disallow: /_next/*
 Disallow: /api/*
-Disallow: /_next/*`);
+Disallow: /_next/*
+Sitemap: https://dev.ibrahimuzun.com/api/sitemap.xml`);
         res.end();
     }
 }
