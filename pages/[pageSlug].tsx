c2921f4c119317e566cef80bfbdc0dbd200ce2cf
İbrahim Uzun <info@ibrahimuzun.com>
2021-03-11 21:56:38 +0300

    page renderer update

--- pages/[pageSlug].tsx
+++ pages/[pageSlug].tsx
@@ -111,33 +111,8 @@ const BlogPosts: React.FC<PostProps & recordMapProps> = ({ post, recordMap }) =>
         <article className="flex-1 my-6 post-container">
           <NotionRenderer
               components={{
-          pageLink: ({
-            href,
-            as,
-            passHref,
-            prefetch,
-            replace,
-            scroll,
-            shallow,
-            locale,
-            ...props
-          }) => (
-            <Link
-              href={href}
-              as={as}
-              passHref={passHref}
-              prefetch={prefetch}
-              replace={replace}
-              scroll={scroll}
-              shallow={shallow}
-              locale={locale}
-            >
-              <a {...props} />
-            </Link>
-          ),
           code: Code,
           collection: Collection,
-          collectionRow: CollectionRow,
           tweet: Tweet,
           modal: Modal,
           pdf: Pdf,
