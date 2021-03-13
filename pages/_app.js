0b13a41f97e61057ddb3be85ab470d6661374ff2
İbrahim Uzun <info@ibrahimuzun.com>
2021-03-13 12:19:53 +0300

    Font edited

--- pages/_app.js
+++ pages/_app.js
@@ -71,6 +71,8 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+<link href="https://fonts.googleapis.com/css2" rel="stylesheet"/>
         <link rel="shortcut icon" href={process.env.NEXT_PUBLIC_FAVICON_URL} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
