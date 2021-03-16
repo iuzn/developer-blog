1695b8a4deed56cf41d2a0ee9a1e023e48dbb862
İbrahim Uzun <info@ibrahimuzun.com>
2021-03-16 23:43:16 +0300

    Update config.js

---
+++ config.js
@@ -0,0 +1,46 @@
+export const config = {
+  notionBlogTableId: process.env.NEXT_PUBLIC_BLOG_ID,
+  notionProjectTableId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  notionCustomPageTableId: process.env.NEXT_PUBLIC_CUSTOM_PAGE_ID,
+  notionBookmarkTableId: process.env.NEXT_PUBLIC_BOOKMARK_ID,
+  profileUrl: process.env.NEXT_PUBLIC_PROFILE_URL,
+  profileDescription: process.env.NEXT_PUBLIC_PROFILE_DESCRIPTION,
+  publicName: process.env.NEXT_PUBLIC_NAME,
+  blogDescription: process.env.NEXT_PUBLIC_BLOG_DESCRIPTION,
+  //Yorum
+  utterancesGitHubRepo: process.env.NEXT_PUBLIC_GITHUB_REPO,
+
+  //Menüler
+  menu1Path: process.env.NEXT_PUBLIC_MENU_1_PATH,
+  menu1Title: process.env.NEXT_PUBLIC_MENU_1_TITLE,
+  menu1Icon: process.env.NEXT_PUBLIC_MENU_1_ICON,
+
+  menu2Path: process.env.NEXT_PUBLIC_MENU_2_PATH,
+  menu2Title: process.env.NEXT_PUBLIC_MENU_2_TITLE,
+  menu2Icon: process.env.NEXT_PUBLIC_MENU_2_ICON,
+
+  menu3Path: process.env.NEXT_PUBLIC_MENU_3_PATH,
+  menu3Title: process.env.NEXT_PUBLIC_MENU_3_TITLE,
+  menu3Icon: process.env.NEXT_PUBLIC_MENU_3_ICON,
+
+  menu4Path: process.env.NEXT_PUBLIC_MENU_4_PATH,
+  menu4Title: process.env.NEXT_PUBLIC_MENU_4_TITLE,
+  menu4Icon: process.env.NEXT_PUBLIC_MENU_4_ICON,
+
+  menu5Path: process.env.NEXT_PUBLIC_MENU_5_PATH,
+  menu5Title: process.env.NEXT_PUBLIC_MENU_5_TITLE,
+  menu5Icon: process.env.NEXT_PUBLIC_MENU_5_ICON,
+
+  menu6Path: process.env.NEXT_PUBLIC_MENU_6_PATH,
+  menu6Title: process.env.NEXT_PUBLIC_MENU_6_TITLE,
+  menu6Icon: process.env.NEXT_PUBLIC_MENU_6_ICON,
+
+  //Linkler
+  twitter: process.env.NEXT_PUBLIC_TWITTER_URL,
+  linkedin: process.env.NEXT_PUBLIC_LINKEDIN_URL,
+  github: process.env.NEXT_PUBLIC_GITHUB_URL,
+  instagram: process.env.NEXT_INSTAGRAM_TWITTER_URL,
+  figma: process.env.NEXT_PUBLIC_FIGMA_URL,
+  youtube: process.env.NEXT_PUBLIC_YOUTUBE_URL,
+  facebook: process.env.NEXT_PUBLIC_FACEBOOK_URL
+}
--- config.tsx
+++
@@ -1,9 +0,0 @@
-export const config = {
-  notionBlogTableId: (process.env.NEXT_PUBLIC_BLOG_ID as string),
-  notionProjectTableId: (process.env.NEXT_PUBLIC_PROJECT_ID as string),
-  notionCustomPageTableId: (process.env.NEXT_PUBLIC_CUSTOM_PAGE_ID as string),
-  notionBookmarkTableId: (process.env.NEXT_PUBLIC_BOOKMARK_ID as string),
-  avatarUrl: (process.env.NEXT_PUBLIC_AVATAR_URL as string),
-  publicName: (process.env.NEXT_PUBLIC_NAME as string),
-  utterancesGitHubRepo:(process.env.NEXT_PUBLIC_GITHUB_REPO as string)
-};
